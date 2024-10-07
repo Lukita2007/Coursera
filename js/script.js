d40229866157ecf59c70a8d8aa91cd3313358ef5
Lukita2007 <Lucasumasrs@gmail.com>
2024-10-07 01:21:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       //
        var homeHtmlToInsertIntoMainPage = insertProperty(
         homeHtmlUrl, 
-          randomCategoryShortName  ,
+          "randomCategoryShortName"  ,
          "{{" + chosenCategoryShortName + "}}"
         );
 
