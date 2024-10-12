d2cba8f1474496e28f460827cd8865bc4e559e84
Lukita2007 <Lucasumasrs@gmail.com>
2024-10-12 13:42:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
-    homeHtmlUrl,
+    homeHtml,
     function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
