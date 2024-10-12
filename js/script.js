7c1c4f656456f134e6a995563e3b8f814138473d
Lukita2007 <Lucasumasrs@gmail.com>
2024-10-12 13:34:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,8 +82,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  homeHtmlUrl,
- buildAndShowHomeHTML(allCategoriesUrl), // ***** <---- TODO: STEP 1: Substitute [...] ******
+  allCategoriesUrl,
+ buildAndShowHomeHTML(), // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
