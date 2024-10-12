02b5dc922b51b4d6b4c395ad92533ba1e092b8db
Lukita2007 <Lucasumasrs@gmail.com>
2024-10-11 22:01:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = chooseRandomCategory
+       var chosenCategoryShortName = chooseRandomCategory(allCategoriesUrl)
            console.log(chosenCategoryShortName)
        
 
